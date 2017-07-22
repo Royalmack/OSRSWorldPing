ea7e8bffffc70c460822af5092c5787900aa5067
Sean Macken <royalmackk@hotmail.co.uk>
2017-07-22 15:53:38 +0100

    Initial Push

---
+++ OSWorldPing/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace OSWorldPing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Location = new System.Drawing.Point(648, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(159, 562);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(3, 515);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ping Worlds";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox2.Location = new System.Drawing.Point(6, 111);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(139, 24);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Member Worlds";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox1.Location = new System.Drawing.Point(6, 69);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 24);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Free Worlds";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(646, 562);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(806, 561);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "OSRS World Ping";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ OSWorldPing/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace OSWorldPing
+{
+    public partial class Form1 : Form
+    {
+        string urlStart = "oldschool";
+        string urlEnd = ".runescape.com";
+
+        string[] freeWorlds = { "1", "8", "16", "26", "35", "81", "82", "83", "84", "85", "93", "94"};
+        string[] memberWorlds = {"2", "3", "4", "5", "6", "7", "9", "10", "11", "12", "13", "14", "15", "17", "18", "19", "20", "21"
+                                ,"22", "23", "24", "25", "27", "28", "29", "30", "31", "32", "33", "34", "36", "37", "38", "39", "40", "41"
+                                ,"42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59"
+                                ,"60", "61", "62", "65", "66", "67", "68", "69", "70", "73", "74", "75", "76", "77", "78", "86", "87", "88"
+                                ,"89", "90", "91", "92", "101", "105"};
+
+        Ping pingSender = new Ping();
+        PingReply reply;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            startPings();
+        }
+
+        private void startPings()
+        {           
+            if (checkBox1.Checked && checkBox2.Checked)
+            {
+                pingAllWorlds();
+            }
+
+            else if (checkBox1.Checked)
+            {
+                pingFreeWorlds();
+            }
+
+            else if(checkBox2.Checked)
+            {
+                pingMemberWorlds();
+            }
+            else
+            {
+                textBox1.Text += "Please select a box!\r\n";
+            }            
+        }
+
+        private void pingFreeWorlds()
+        {
+            foreach(string w in freeWorlds)
+            {
+                reply = pingSender.Send(urlStart + w + urlEnd);
+                textBox1.AppendText("World " + w + "\t Ping: " + reply.RoundtripTime + "ms\r\n");
+            }            
+        }
+
+        private void pingMemberWorlds()
+        {  
+            foreach (string w in memberWorlds)
+            {
+                reply = pingSender.Send(urlStart + w + urlEnd);
+                textBox1.AppendText("World " + w + "\t Ping: " + reply.RoundtripTime + "ms\r\n");
+            }
+        }
+
+        private void pingAllWorlds()
+        {
+            //Merge both lists and order by value so 1 - ...
+            string[] merged = freeWorlds.Concat(memberWorlds).ToArray();
+            var allWorlds = from x in merged orderby x.Length, x select x;
+
+            foreach (string w in allWorlds)
+            {
+                reply = pingSender.Send(urlStart + w + urlEnd);
+                textBox1.AppendText("World " + w + "\t Ping: " + reply.RoundtripTime + "ms\r\n");
+            }
+        }
+    }
+}
